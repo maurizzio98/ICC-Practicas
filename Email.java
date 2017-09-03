9518319b7aca1df6a5cf555407a9d823fdf3af0f
Mauricio <mauriciochavez@ciencias.unam.mx>
2017-09-02 19:55:04 -0500

    Email.java

---
+++ Email.java
@@ -0,0 +1,52 @@
+import java.util.Scanner ;
+
+/**
+ * Programa que extraiga la dirección electrónica del dominio ciencias de un texto.
+ * @author Mauricio Chávez Olea
+ */
+
+public class Email {
+    public static void main(String[] pps) {
+
+      Scanner in = new Scanner (System.in) ;
+      String texto, email, textominus ;
+      int posicionciencias, posicionarroba ;
+
+      System.out.println("Dame un texto:") ;
+      texto = in.nextLine() ;
+
+
+      texto = texto.trim() ;
+      textominus = texto.toLowerCase() ;
+      posicionarroba= textominus.indexOf("@") ;
+
+      if(posicionarroba<0){ //ME FALTAN CONDICIONES
+
+      System.out.println("No hay direccion electronica");
+
+    } //ACABA RESTRICCION NO EMAIL
+
+      else {
+
+        posicionciencias= textominus.indexOf("@ciencias.unam.mx") ;
+
+        if(posicionciencias<0){ //Me FALTA METODO PARA CONSEGUIR CORREO
+
+          email = texto ; //CORREO NO ES IGUAL A TEXTO :v
+
+          System.out.println("La direccion es " + email +" pero no está en el dominio de ciencias");
+        } //ACABA RESTRICCION NO CORREO DE CIENCIAS
+
+        else {
+
+            System.out.println(texto);
+            System.out.println(posicionciencias);
+
+
+        } //ACABA ELSE SI CORREO CIENCIAS
+
+      } //ACABA ELSE SÍ CORREO ELECTRÓNICO
+
+
+    } //NO BORRAR - PUBLIC CLASS
+} //NO BORRAR - MAIN()
