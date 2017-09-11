bc28516096e7636fd71ccfc3ae20fc7af13c8842
Mauricio <mauriciochavez@ciencias.unam.mx>
2017-09-10 19:47:44 -0500

    Email

--- Email.java
+++ Email.java
@@ -9,8 +9,9 @@ public class Email {
     public static void main(String[] pps) {
 
       Scanner in = new Scanner (System.in) ;
-      String texto, email, textominus ;
-      int posicionciencias, posicionarroba ;
+      String texto, email, textominus, textoespaciofinal,textoespaciadoinicio ;
+      String inicioespaciado = " " ;
+      int posicionciencias, posicionarroba, pos1, pos2;
 
       System.out.println("Dame un texto:") ;
       texto = in.nextLine() ;
@@ -30,18 +31,45 @@ public class Email {
 
         posicionciencias= textominus.indexOf("@ciencias.unam.mx") ;
 
-        if(posicionciencias==-1){ //Me FALTA METODO PARA CONSEGUIR CORREO
+//NO ES UN CORREO DE CIENCIAS
+        if(posicionciencias==-1){
 
-          email = texto ; //CORREO NO ES IGUAL A TEXTO :v
+          textoespaciadoinicio = inicioespaciado.concat(textominus);
+          textoespaciofinal = textoespaciadoinicio.concat(" ");
+          pos1 = textoespaciofinal.lastIndexOf(" ",posicionarroba) ;
+          pos2 = textoespaciofinal.indexOf(" ",posicionarroba) ;
+          email = textoespaciofinal.substring(pos1,pos2) ;
 
-          System.out.println("La direccion es " + email +" pero no está en el dominio de ciencias");
-        } //ACABA RESTRICCION NO CORREO DE CIENCIAS
+            if (pos1==posicionarroba){
+            System.out.println("Esto es una dirección de correo inválida: Falta algo antes de la arroba (@)");
+            }
 
+            else {
+              System.out.println("La direccion es" + email + " pero no está en el dominio de ciencias.");
+
+            }
+
+}
+ //ACABA RESTRICCION NO CORREO DE CIENCIAS
+
+//CORREO DE CIENCIAS
         else {
 
-            System.out.println("Este es un dominio @ciencias");
-            System.out.println(posicionciencias);
+          textoespaciadoinicio = inicioespaciado.concat(textominus);
+          textoespaciofinal = textoespaciadoinicio.concat(" ");
+          pos1 = textoespaciofinal.lastIndexOf(" ",posicionarroba) ;
+          pos2 = textoespaciofinal.indexOf(" ",posicionarroba) ;
+          email = textoespaciofinal.substring(pos1,pos2) ;
 
+//CORREO DE CIENCAS INVÁLIDO
+            if (pos1==posicionarroba){
+            System.out.println("Esto es una dirección de CORREO CIENCIAS inválida: Falta algo antes de la arroba (@)");
+            }
+//CORREO DE CIENCAS VÁLIDO
+            else {
+              System.out.println("La direccion es" + email + " y sí está en el dominio ciencias");
+
+            }
 
         } //ACABA ELSE SI CORREO CIENCIAS
 
